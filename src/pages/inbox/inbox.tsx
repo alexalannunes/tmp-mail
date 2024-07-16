fa2a6e2080feec23df9996c090db3d7e0694cf2f
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-16 16:19:02 -0300

    prepare message view

--- src/pages/inbox/inbox.tsx
+++ src/pages/inbox/inbox.tsx
@@ -1,12 +1,24 @@
 import { Box } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { loggedApi } from "../../infra/http";
 
 export function InboxPage() {
+  const { data, isPending } = useQuery({
+    queryKey: ["messages"],
+    queryFn: async () => {
+      const req = await loggedApi.get("/messages");
+      const data = req.data;
+      console.log(data);
+      return data;
+    },
+  });
+
   return (
     <Box p={7}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nulla
-      voluptatem illum repudiandae excepturi fuga est, perspiciatis architecto
-      amet maiores, fugiat accusamus, veniam nobis? Expedita non adipisci quia.
-      Aperiam, earum.
+      {isPending && <>loading...</>}
+      {data?.["hydra:member"]?.length} message
+      <br />
+      {data?.["hydra:member"]?.[0]?.intro}
     </Box>
   );
 }
