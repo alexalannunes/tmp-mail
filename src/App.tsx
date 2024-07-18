f5db80742148d7f7e32f34a6c24cc69767822682
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-18 15:33:22 -0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import { MdFavorite } from "react-icons/md";
 import { Aside } from "./ui/aside/aside";
 import { Header } from "./ui/header/header";
-import { Outlet } from "react-router-dom";
 function App() {
   return (
     <Flex minH={"100vh"}>
@@ -16,6 +17,34 @@ function App() {
           }}
         >
           <Outlet />
+        </Box>
+        <Box px={7} py={2}>
+          <Flex
+            gap={2}
+            alignItems={"center"}
+            fontSize={"14px"}
+            justifyContent={"center"}
+          >
+            <Text
+              color={"gray.700"}
+              _dark={{
+                color: "gray.400",
+              }}
+            >
+              Made with
+            </Text>
+            <MdFavorite color="red" /> {/* setup vercel url */}
+            <Link
+              href="https://docs.mail.tm/?ref=tmp.mail.vercel.app"
+              target="_blank"
+              color={"teal.500"}
+              _dark={{
+                color: "teal.400",
+              }}
+            >
+              https://docs.mail.tm
+            </Link>
+          </Flex>
         </Box>
       </Flex>
     </Flex>
