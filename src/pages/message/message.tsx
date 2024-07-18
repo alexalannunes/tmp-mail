0b2a4632d4bee04e500b584a6a16b086c3ae7b62
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-18 12:28:10 -0300

    view message with skeleton

--- src/pages/message/message.tsx
+++ src/pages/message/message.tsx
@@ -1,13 +1,30 @@
-import { Box, Spinner } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import { MdDeleteOutline, MdOutlineMessage } from "react-icons/md";
 import { loggedApi } from "../../infra/http";
 import { MessageView } from "../../mock/messages";
 
 export function MessagePage() {
   const { messageId } = useParams();
 
-  const { data, isPending } = useQuery<MessageView>({
+  const {
+    data: message,
+    isPending,
+    isFetching,
+    refetch,
+  } = useQuery<MessageView>({
     queryKey: ["message", messageId],
     queryFn: async () => {
       const req = await loggedApi.get(`/messages/${messageId}`);
@@ -18,21 +35,73 @@ export function MessagePage() {
     enabled: !!messageId,
   });
 
-  return (
-    <Box p={7}>
-      {isPending && <Spinner />}
+  const isLoading = isPending || isFetching;
 
-      <Box bg="white" p={3}>
-        {data?.html.map((html, index) => (
-          <div
-            style={{ color: "white" }}
-            key={index}
-            dangerouslySetInnerHTML={{
-              __html: html,
-            }}
-          />
-        ))}
+  return (
+    <>
+      <Button onClick={() => refetch()}>Refetch</Button>
+      <Box p={7}>
+        <Flex alignItems={"center"} justifyContent={"space-between"}>
+          <Skeleton height="14px" w={"400px"} isLoaded={!isLoading}>
+            <Text fontWeight={"semibold"}>{message?.subject}</Text>
+          </Skeleton>
+          <HStack>
+            <Button
+              size="sm"
+              variant={"outline"}
+              colorScheme="cyan"
+              leftIcon={<MdOutlineMessage />}
+            >
+              Mark as unread
+            </Button>
+            <Button
+              size="sm"
+              variant={"outline"}
+              colorScheme="red"
+              leftIcon={<MdDeleteOutline />}
+            >
+              Delete
+            </Button>
+          </HStack>
+        </Flex>
+
+        <Box mt={4} padding="6" bg="white" rounded={"md"}>
+          <HStack w={"full"} spacing={6}>
+            <SkeletonCircle size={"12"} flexShrink={0} isLoaded={!isLoading}>
+              <Avatar name={message?.from.name || message?.from.address} />
+            </SkeletonCircle>
+            <Stack w={"full"}>
+              <Skeleton height="16px" isLoaded={!isLoading}>
+                <Text color={"cyan.500"}>{message?.from.name}</Text>
+              </Skeleton>
+              <Skeleton height="16px" isLoaded={!isLoading}>
+                <Text fontSize={"14px"} color={"gray.500"}>
+                  {message?.from.address}
+                </Text>
+              </Skeleton>
+            </Stack>
+          </HStack>
+          <SkeletonText
+            noOfLines={4}
+            mt={10}
+            spacing="4"
+            skeletonHeight="4"
+            isLoaded={!isLoading}
+          >
+            <Box>
+              {message?.html.map((html, index) => (
+                <Box
+                  color="gray.800"
+                  key={index}
+                  dangerouslySetInnerHTML={{
+                    __html: html,
+                  }}
+                />
+              ))}
+            </Box>
+          </SkeletonText>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
