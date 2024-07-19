611a835a11fba12f7e1d092f165859062755ef52
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-19 16:03:05 -0300

    remove refetch button

--- src/pages/message/message.tsx
+++ src/pages/message/message.tsx
@@ -23,7 +23,6 @@ export function MessagePage() {
     data: message,
     isPending,
     isFetching,
-    refetch,
   } = useQuery<MessageView>({
     queryKey: ["message", messageId],
     queryFn: async () => {
@@ -38,70 +37,67 @@ export function MessagePage() {
   const isLoading = isPending || isFetching;
 
   return (
-    <>
-      <Button onClick={() => refetch()}>Refetch</Button>
-      <Box p={7}>
-        <Flex alignItems={"center"} justifyContent={"space-between"}>
-          <Skeleton height="14px" w={"400px"} isLoaded={!isLoading}>
-            <Text fontWeight={"semibold"}>{message?.subject}</Text>
-          </Skeleton>
-          <HStack>
-            <Button
-              size="sm"
-              variant={"outline"}
-              colorScheme="cyan"
-              leftIcon={<MdOutlineMessage />}
-            >
-              Mark as unread
-            </Button>
-            <Button
-              size="sm"
-              variant={"outline"}
-              colorScheme="red"
-              leftIcon={<MdDeleteOutline />}
-            >
-              Delete
-            </Button>
-          </HStack>
-        </Flex>
-
-        <Box mt={4} padding="6" bg="white" rounded={"md"}>
-          <HStack w={"full"} spacing={6}>
-            <SkeletonCircle size={"12"} flexShrink={0} isLoaded={!isLoading}>
-              <Avatar name={message?.from.name || message?.from.address} />
-            </SkeletonCircle>
-            <Stack w={"full"}>
-              <Skeleton height="16px" isLoaded={!isLoading}>
-                <Text color={"cyan.500"}>{message?.from.name}</Text>
-              </Skeleton>
-              <Skeleton height="16px" isLoaded={!isLoading}>
-                <Text fontSize={"14px"} color={"gray.500"}>
-                  {message?.from.address}
-                </Text>
-              </Skeleton>
-            </Stack>
-          </HStack>
-          <SkeletonText
-            noOfLines={4}
-            mt={10}
-            spacing="4"
-            skeletonHeight="4"
-            isLoaded={!isLoading}
+    <Box p={7}>
+      <Flex alignItems={"center"} justifyContent={"space-between"}>
+        <Skeleton height="14px" w={"400px"} isLoaded={!isLoading}>
+          <Text fontWeight={"semibold"}>{message?.subject}</Text>
+        </Skeleton>
+        <HStack>
+          <Button
+            size="sm"
+            variant={"outline"}
+            colorScheme="cyan"
+            leftIcon={<MdOutlineMessage />}
           >
-            <Box>
-              {message?.html.map((html, index) => (
-                <Box
-                  color="gray.800"
-                  key={index}
-                  dangerouslySetInnerHTML={{
-                    __html: html,
-                  }}
-                />
-              ))}
-            </Box>
-          </SkeletonText>
-        </Box>
+            Mark as unread
+          </Button>
+          <Button
+            size="sm"
+            variant={"outline"}
+            colorScheme="red"
+            leftIcon={<MdDeleteOutline />}
+          >
+            Delete
+          </Button>
+        </HStack>
+      </Flex>
+
+      <Box mt={4} padding="6" bg="white" rounded={"md"}>
+        <HStack w={"full"} spacing={6}>
+          <SkeletonCircle size={"12"} flexShrink={0} isLoaded={!isLoading}>
+            <Avatar name={message?.from.name || message?.from.address} />
+          </SkeletonCircle>
+          <Stack w={"full"}>
+            <Skeleton height="16px" isLoaded={!isLoading}>
+              <Text color={"cyan.500"}>{message?.from.name}</Text>
+            </Skeleton>
+            <Skeleton height="16px" isLoaded={!isLoading}>
+              <Text fontSize={"14px"} color={"gray.500"}>
+                {message?.from.address}
+              </Text>
+            </Skeleton>
+          </Stack>
+        </HStack>
+        <SkeletonText
+          noOfLines={4}
+          mt={10}
+          spacing="4"
+          skeletonHeight="4"
+          isLoaded={!isLoading}
+        >
+          <Box>
+            {message?.html.map((html, index) => (
+              <Box
+                color="gray.800"
+                key={index}
+                dangerouslySetInnerHTML={{
+                  __html: html,
+                }}
+              />
+            ))}
+          </Box>
+        </SkeletonText>
       </Box>
-    </>
+    </Box>
   );
 }
