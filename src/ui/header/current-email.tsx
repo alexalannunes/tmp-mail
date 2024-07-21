389efc9f4de3ba14ce6246d85e3807c14e85a318
Alex Alan Nunes <alexalannunes@gmail.com>
2024-07-21 20:08:41 -0300

    success color when copy

--- src/ui/header/current-email.tsx
+++ src/ui/header/current-email.tsx
@@ -5,9 +5,10 @@ import {
   SlideFade,
   Tooltip,
   useClipboard,
+  useColorMode,
 } from "@chakra-ui/react";
 import { FaCheck } from "react-icons/fa6";
-
+import { MdMailOutline } from "react-icons/md";
 import { useAccount } from "../../data/context/account-context";
 
 // we set a created email if no email was created
@@ -16,19 +17,41 @@ const email = "loading@email.com";
 export function HeaderCurrentMail() {
   const account = useAccount();
 
+  const { colorMode } = useColorMode();
+
   const { onCopy, hasCopied } = useClipboard(account?.address || "");
+
+  const getColorOnCopy = () => {
+    if (colorMode === "dark") {
+      if (hasCopied) return "green.300";
+
+      return "white";
+    }
+    if (hasCopied) return "green.400";
+
+    return "gray.700";
+  };
+
+  const colorOnCopy = getColorOnCopy();
+
   return (
     <Flex alignItems={"center"} gap={2}>
+      <Icon as={MdMailOutline} color={colorOnCopy} />
       <Tooltip
         placement="bottom"
         label="Your temporary email address, click to copy"
       >
-        <Button variant={"ghost"} size={"sm"} onClick={onCopy}>
+        <Button
+          variant={"ghost"}
+          size={"sm"}
+          onClick={onCopy}
+          color={colorOnCopy}
+        >
           {account?.address ?? email}
         </Button>
       </Tooltip>
       <SlideFade offsetX={-20} offsetY={0} in={hasCopied}>
-        <Icon as={FaCheck} color={"green.500"} _dark={{ color: "green.400" }} />
+        <Icon as={FaCheck} color={"green.400"} _dark={{ color: "green.300" }} />
       </SlideFade>
     </Flex>
   );
